4ad1d700cbcbd42bd2dbd72dbcedb7715ab5a828
Thomas <35410212+th-km@users.noreply.github.com>
2025-08-05 11:49:37 +0200

    Issue with vite optimizeDeps

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const query = groq`*[]`
+const { data } = await useSanityQuery(query)
+</script>
+
+<template>
+  <div>
+    <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', '@nuxtjs/sanity'],
+  sanity: {
+    projectId: process.env.NUXT_SANITY_PROJECT_ID,
+    dataset: process.env.NUXT_SANITY_DATASET,
+    apiVersion: process.env.NUXT_SANITY_API_VERSION,
+    useCdn: false
+  }
+  //vite: {
+  //  optimizeDeps: {
+  //    include: ['react-compiler-runtime', 'react', 'react-dom']
+  //  }
+  //}
 })
